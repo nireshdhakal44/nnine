b2dc943f2e815fc3c5e4e6fdd3a58dd0f188c330
nireshdhakal44 <39344465+nireshdhakal44@users.noreply.github.com>
2024-02-10 08:50:19 +0545

    added route

--- src/App.js
+++ src/App.js
@@ -41,10 +41,9 @@ function App() {
             <Route path="/cardlist" element={<CardList />} />
             <Route path="/about" element={<About />} />
             <Route path="/trainers" element={<TrainersPage/>} />
+            <Route path="/home" element={<Home/>} />
 
-
-
-          </Routes>
+         </Routes>
         </ScrollToTop>
       </Router>
     </>
